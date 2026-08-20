225029ded24e214bf54f57b938135b5ce522cca5
Oliver Henry <271880414+oliverhenry-dev@users.noreply.github.com>
2026-08-20 14:12:39 +0630

    feat: added product list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import Footer from "./components/Footer"
+import Footer from "./components/Footer.jsx"
 import Header from "./components/Header"
 import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import products from "../products.js"
+import { Col, Row } from "react-bootstrap"
+import Porduct from "../components/Product"
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Porduct product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
